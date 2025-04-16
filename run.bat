6ef4c992b8cbc5d6f06f7d68af41114197fb3754
mwoodall-tamu <mwoodall@tamu.edu>
2025-04-16 13:08:01 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Welcome to Python 3D.
+echo Welcome to Simple 3D.
 echo -- Instructions / Information --
 echo use the W A S D keys to move to camera
 echo use the SPACE and LSHIFT keys to go up and down
@@ -9,9 +9,9 @@ echo use ESC to exit the game. (Important)
 echo --------------------------------
 pause
 echo Setting up python environment
-python3 -m venv venv
+%UserProfile%\AppData\Local\Microsoft\WindowsApps\python3.exe -m venv venv
 echo Setup complete!
-call venv/Scripts/activate
+call venv\Scripts\activate
 echo Loading required Libraries. This might take a while . . .
 pip install -r requirments.txt
 echo Libraries loaded, starting program.
