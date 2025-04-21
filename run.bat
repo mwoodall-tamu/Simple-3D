611984e74af3ead31fa16a241156e987871f771d
mwoodall-tamu <mwoodall@tamu.edu>
2025-04-21 16:19:22 -0500

    Fixed run.bat to optimize for new approach

--- run.bat
+++ run.bat
@@ -8,11 +8,5 @@ echo use the F key to toggle fullscreen
 echo use ESC to exit the game. (Important)
 echo --------------------------------
 pause
-echo Setting up python environment
-%UserProfile%\AppData\Local\Microsoft\WindowsApps\python3.exe -m venv venv
-echo Setup complete!
 call venv\Scripts\activate
-echo Loading required Libraries. This might take a while . . .
-pip install -r requirments.txt
-echo Libraries loaded, starting program.
 python3 main.py
